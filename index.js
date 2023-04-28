676a09ed46fdea7d285824626f10cebe018fb820
Rubi Alvarado <kimjeonrubi7@gmail.com>
2023-04-28 12:17:29 -0600

    cv portafolio nuevo

--- index.js
+++ index.js
@@ -57,34 +57,32 @@ fetchData(API);
 
 // }
 
+
 const misCards = (responseRepos) => {
     let cardAcumuladas = "";
     responseRepos.forEach(element => {
-        let card = `
-        <div class = "col-12 col-sm-6 col-lg-4
-         text-center my-2  text-dark color  ">
-             <div class="card  bg-dark bg-opacity-10 m-8" style="height: 36rem; ">                 
-                 <div class="card-body divs  ">
-                    <h4 class="card-title my-3 ">Name: ${element.name}</h4>
-                    <img src="./img/yoongi2.jpg" class="card-img-top ms-5" alt="img de una persona" style="width: 12rem;>
-                    <p class="card-text my-2  ">
-                    Full_Name:  ${element.full_name} <p>
-                    <br>
-                    <p class="card-text my-2 ">
-                    Description:: ${element.description}
-                    </p>
-                    <p class="card-text my-2 ">
-                    <a href="${element.html_url}" class="card-text my-2 btn btn-outline-dark"> Ver repositorio
-                    </a>
-                    </p>
-                 </div>
-             </div>
-         </div>`
+        let card = `<div >
+         <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-6">
+           <div class="md:flex ">
+            <div class="md:shrink-0">
+              <img class="h-40 w-full object-cover md:h-full md:w-48" src="./img/yoongi2.jpg" alt="Modern building architecture">
+        </div>
+      <div class="p-8">
+       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"> <h3>Name:${element.name}</h3></div>
+       <p class="mt-2 text-slate-500">Full_Name:  ${element.full_name} </p>
+       <p>
+       Description:: ${element.description}
+       </p>
+       <p>
+       <a href="${element.html_url}" class="block mt-3 text-lg leading-tight claseA font-medium text-black hover:underline">Ver repositorio</a>
+    </div>
+  </div>
+</div>
+</div`
         cardAcumuladas += card;
-
     });
-
     container.innerHTML = cardAcumuladas;
-
-    
 }
+
+
+//class="grid grid-rows-5 grid-flow-col
